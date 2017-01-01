2abf484a2c8075d569a47a19455f1d22fb46bbf6
Sergii Zagryvyi <serzhcello@gmail.com>
2017-01-01 15:12:23 +0200

    add factorial

--- src/main/java/ua/com/serzh/sortingDismantle/Factorial.java
+++ src/main/java/ua/com/serzh/sortingDismantle/Factorial.java
@@ -6,17 +6,29 @@ package ua.com.serzh.sortingDismantle;
 public class Factorial {
     public static void main(String[] args) {
 
-        System.out.println(factorial(5));
+        int num = 0;
+        int result = factorial(num);
+//        int result = factorRecurs(num);
+        System.out.println(result);
     }
 
-    static int factorial(int num) {
+    private static int factorial(int num) {
+        int fact = 1;
+
+        for (; num > 0; fact *= num--) ;
+
+        return fact;
+    }
+
+    private static int factorRecurs(int num) {
+        return (num == 0) ? 1 : num * factorRecurs(num - 1);
+    }
+
+    static int factorRecurs2(int num) {
         if (num == 1) {
             return 1;
         }
         return num * factorial(num - 1);
     }
 
-    static int factorial2(int n) {
-        return (n == 1) ? n : n * factorial2(n - 1);
-    }
 }
