63f95ba1fafe0724bbc7540c534151d2b990b37c
Sergii Zagryvyi <serzhcello@gmail.com>
2017-01-22 13:44:37 +0200

    add QuickFind

---
+++ src/main/java/ua/com/serzh/coursera/Temp.java
@@ -0,0 +1,7 @@
+package ua.com.serzh.coursera;
+
+/**
+ * Created by Serzh on 1/14/17.
+ */
+public class Temp {
+}
---
+++ src/main/java/ua/com/serzh/coursera/week_1/UF.java
@@ -0,0 +1,20 @@
+package ua.com.serzh.coursera.week_1;
+
+/**
+ * Created by Serzh on 1/14/17.
+ */
+public class UF {
+    private int n;
+
+    public UF(int n) {
+
+        this.n = n;
+    }
+
+    public static void main(String[] args) {
+//        int N = StdIn.readIn();
+//        UF uf = new UF(N);
+    }
+
+
+}
